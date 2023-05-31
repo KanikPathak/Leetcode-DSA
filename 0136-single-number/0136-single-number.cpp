e65d7b583287e812294cf7a396bf7931d99abd9c
guyfrompluto <120249077+guyfrompluto@users.noreply.github.com>
2023-06-01 01:41:02 +0530

    Time: 20 ms (70.25%), Space: 16.8 MB (99.17%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
+        int an=0;
       for(int i=0;i<nums.size();i++)
-          ans=ans^nums[i];
-      return ans;
+          an=an^nums[i];
+      return an;
     }
 };
