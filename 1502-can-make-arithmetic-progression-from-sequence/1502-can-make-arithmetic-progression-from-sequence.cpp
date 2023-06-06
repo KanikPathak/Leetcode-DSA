f457f8edd22d3053d3f57a45b424bbc2bef9fd38
guyfrompluto <120249077+guyfrompluto@users.noreply.github.com>
2023-06-06 12:19:50 +0530

    Time: 9 ms (22.98%), Space: 9.1 MB (66.22%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -7,7 +7,7 @@ public:
     sort(arr.begin(),arr.end());
 
    int d= arr[1]-arr[0];
-    for(int i=0;i<arr.size()-1;i++){
+    for(int i=1;i<arr.size()-1;i++){
         if(arr[i+1]-arr[i] != d)
         return 0;
     }
