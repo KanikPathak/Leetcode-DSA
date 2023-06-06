fb034639f68a477c438b7e7faf5f92726dd01e4e
guyfrompluto <120249077+guyfrompluto@users.noreply.github.com>
2023-06-06 12:14:11 +0530

    Time: 9 ms (22.98%), Space: 9.1 MB (66.22%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+    // sbse chhota diff nikal,
+    // agar sare diff us d ke multiple hain to ap hai
+
+    sort(arr.begin(),arr.end());
+
+   int d= arr[1]-arr[0];
+    for(int i=0;i<arr.size()-1;i++){
+        if(arr[i+1]-arr[i] != d)
+        return 0;
+    }
+    return 1;
+
+
+
+    }
+};
